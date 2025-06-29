c22a3c7412cb62b0d5d98368ddd56ff4ef317f03
hrdk94 <goyalhardik220@gmail.com>
2025-06-30 01:14:53 +0530

    some changes

--- src/msgbox.jsx
+++ src/msgbox.jsx
@@ -10,10 +10,11 @@ function MsgBox({sender, receiver, text}){
     }
     return(
         <div style={styles}>
-            <button onClick={printHello}>Click me!</button>
+            
              <h3>Message from : {sender}</h3>
             <h3>Message to : {receiver}</h3>
             <p>Message is : {text} </p>
+            <button onClick={printHello}>Click me!</button>
         </div>
     )
 }
